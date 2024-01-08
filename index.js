f70b4ff040109cb092e42e1587200056724f2f71
AhmedMonem122 <ahmed.monem90@yahoo.com>
2024-01-08 21:33:00 +0200

    Add Cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const todosRouter = require("./routes/todosRouter");
 const userRouter = require("./routes/userRouter");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/todos", todosRouter);
